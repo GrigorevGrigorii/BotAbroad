97c9736ebe98833102dbce686f1d3212c72bcf92
GrigorevGrigorii <nigddd@gmail.com>
2021-03-15 21:50:25 +0300

    Add word splitting of large messages (issue 14)

--- app.py
+++ app.py
@@ -151,7 +151,20 @@ def respond():
             bot.sendMessage(chat_id, "Минуточку, бот ищет данные...")
             try:
                 def chunk_string(string, length):
-                    return (string[0 + i:length + i] for i in range(0, len(string), length))
+                    """Возвращает string разбитую по части длины не больше length по словам"""
+                    if len(string) <= length:
+                        return [string]
+
+                    chunked_string = ['']
+                    for word in string.split(' '):
+                        if len(word) > length:
+                            raise AttributeError('Length should be more than the longest word!')
+
+                        if len(chunked_string[-1] + ' ' + word) <= length:
+                            chunked_string[-1] += (' ' + word) if chunked_string[-1] != '' else word
+                        else:
+                            chunked_string.append(word)
+                    return chunked_string
 
                 markup = telegram.ReplyKeyboardRemove()
                 if "borders_command" in chat_state.state:
